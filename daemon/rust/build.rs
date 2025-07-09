9b5228cec6b16ec474ca8ff60de01b43f7a963ba
zzaolv <2863436223@qq.com>
2025-07-08 18:27:49 -0700

    fix

--- daemon/rust/build.rs
+++ daemon/rust/build.rs
@@ -3,10 +3,10 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         // logic_controller.rs 包含 LogicController 的实现，必须被编译
-        .file("src/logic_controller.rs") 
+//        .file("src/logic_controller.rs") 
         .flag_if_supported("-std=c++17")
         .compile("crfzit_cxx_bridge");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=src/logic_controller.rs");
+//    println!("cargo:rerun-if-changed=src/logic_controller.rs");
 }
