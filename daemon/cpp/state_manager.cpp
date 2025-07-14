afefb88800904df3a19864df923c903ac0f73672
zzaolv <2863436223@qq.com>
2025-07-14 02:30:38 -0700

    fix

--- daemon/cpp/state_manager.cpp
+++ daemon/cpp/state_manager.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <stdexcept>
 #include <array>
+#include <sstream> // <<< 新增这一行
 
 #define LOG_TAG "crfzitd_state"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
